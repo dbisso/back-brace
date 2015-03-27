5e6c541de098b2a578b9964e5b3940b20035a1cb
Dan Bissonnet <dan@danisadesigner.com>
2015-03-27 12:42:44 +0000

    Enforce PSR2 standards

--- src/DatabaseDumper.php
+++ src/DatabaseDumper.php
@@ -1,104 +1,118 @@
 <?php
+
 namespace DBisso\BackBrace;
 
 /**
- * Creates a resource that streams a dump of a MySQL database
- *
- * @package BackBrace
+ * Creates a resource that streams a dump of a MySQL database.
  */
-class DatabaseDumper {
-	/**
-	 * The stream representing the database dump
-	 * @var resource
-	 */
-	private $dumpStream;
+class DatabaseDumper
+{
+    /**
+     * The stream representing the database dump.
+     *
+     * @var resource
+     */
+    private $dumpStream;
 
-	/**
-	 * The path to the mysqldump binary
-	 * @var string
-	 */
-	private $mysqldumpBin;
+    /**
+     * The path to the mysqldump binary.
+     *
+     * @var string
+     */
+    private $mysqldumpBin;
 
-	/**
-	 * Database connection details
-	 * @var array
-	 */
-	private $connectionConfig;
+    /**
+     * Database connection details.
+     *
+     * @var array
+     */
+    private $connectionConfig;
 
-	/**
-	 * The actual dumping process
-	 * @var resource
-	 */
-	private $dumpProcess;
+    /**
+     * The actual dumping process.
+     *
+     * @var resource
+     */
+    private $dumpProcess;
 
-	/**
-	 * Constructor
-	 * @param array $connectionConfig Database connection details
-	 */
-	public function __construct( array $connectionConfig ) {
-		$this->connectionConfig = $connectionConfig;
-		$this->mysqldumpBin = $this->getDumpBin();
+    /**
+     * Constructor.
+     *
+     * @param array $connectionConfig Database connection details
+     */
+    public function __construct(array $connectionConfig)
+    {
+        $this->connectionConfig = $connectionConfig;
+        $this->mysqldumpBin = $this->getDumpBin();
 
-		// $this->dumpStream = fopen( sys_get_temp_dir() . '/gotyourback-dump-temp', 'w+' );
-		// var_dump(sys_get_temp_dir() . '/gotyourback-dump-temp');
-		// $this->dumpStream = tmpfile();
-	}
+        // $this->dumpStream = fopen( sys_get_temp_dir() . '/gotyourback-dump-temp', 'w+' );
+        // var_dump(sys_get_temp_dir() . '/gotyourback-dump-temp');
+        // $this->dumpStream = tmpfile();
+    }
 
-	/**
-	 * Trigger the dumping
-	 * @return stream The stream of the dump
-	 */
-	public function dump() {
-		$this->process = $this->getDumpProcess();
+    /**
+     * Trigger the dumping.
+     *
+     * @return stream The stream of the dump
+     */
+    public function dump()
+    {
+        $this->process = $this->getDumpProcess();
 
-		if ( false === $this->process ) {
-			return false;
-		}
+        if (false === $this->process) {
+            return false;
+        }
 
-		return $this->dumpStream;
-	}
+        return $this->dumpStream;
+    }
 
-	/**
-	 * Get the dump process
-	 * @return resource Resource representing the mysqldump command
-	 */
-	private function getDumpProcess() {
-		$filename = tempnam( sys_get_temp_dir(), 'gyb' );
-		$cmd      = system( $this->getDumpCommand() . ' > ' . escapeshellcmd( $filename ) );
+    /**
+     * Get the dump process.
+     *
+     * @return resource Resource representing the mysqldump command
+     */
+    private function getDumpProcess()
+    {
+        $filename = tempnam(sys_get_temp_dir(), 'gyb');
+        $cmd      = system($this->getDumpCommand().' > '.escapeshellcmd($filename));
 
-		$this->dumpStream = fopen( $filename, 'r+b' );
+        $this->dumpStream = fopen($filename, 'r+b');
 
-		return $cmd;
-	}
+        return $cmd;
+    }
 
-	private function getDumpCommand() {
-		$mysqldumpBin = $this->getDumpBin();
+    private function getDumpCommand()
+    {
+        $mysqldumpBin = $this->getDumpBin();
 
-		return escapeshellcmd( "$mysqldumpBin -u{$this->connectionConfig['user']} -p{$this->connectionConfig['password']} -h{$this->connectionConfig['host']} -P{$this->connectionConfig['port']} {$this->connectionConfig['database']}" );
-	}
+        return escapeshellcmd("$mysqldumpBin -u{$this->connectionConfig['user']} -p{$this->connectionConfig['password']} -h{$this->connectionConfig['host']} -P{$this->connectionConfig['port']} {$this->connectionConfig['database']}");
+    }
 
-	/**
-	 * Get the binary for mysqldump
-	 * @return string Path to mysqldump
-	 */
-	private function getDumpBin() {
-		exec( 'which mysqldump', $exec, $return );
+    /**
+     * Get the binary for mysqldump.
+     *
+     * @return string Path to mysqldump
+     */
+    private function getDumpBin()
+    {
+        exec('which mysqldump', $exec, $return);
 
-		if ( $return === 0 ) {
-			return $exec[0];
-		}
+        if ($return === 0) {
+            return $exec[0];
+        }
 
-		throw new Exception("Command mysqldump not found", 1);
-	}
+        throw new Exception('Command mysqldump not found', 1);
+    }
 
-	/**
-	 * Tidy up our connections.
-	 */
-	public function __destruct() {
-		// if ( $this->dumpStream ) {
-		// }
+    /**
+     * Tidy up our connections.
+     */
+    public function __destruct()
+    {
+        // if ( $this->dumpStream ) {
+        // }
 
-		// proc_close($this->process);
-		// fclose($this->dumpStream);
-	}
+        // proc_close($this->process);
+        // fclose($this->dumpStream);
+    }
 }
